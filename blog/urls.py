623997fc8554498e66eda3c462abfd172a7b4738
borna <borna@cafebazaar.ir>
2021-02-26 01:28:38 +0330

    Add author filtered blog page

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from blog.views import all_posts
 
 urlpatterns = [
-    path('', all_posts)
+    path('', all_posts),
+    path('<author>', all_posts)
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,13 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 
 from blog.models import Post
 
 
-def all_posts(request):
-    template = loader.get_template('blog/index.html')
-    posts = Post.objects.all()
-    content = template.render(context={'posts': posts}, request=request)
-    return HttpResponse(content=content)
+def all_posts(request, author=None):
+    if author:
+        posts = Post.objects.filter(author=author)
+    else:
+        posts = Post.objects.all()
+    return render(context={'posts': posts},
+                  template_name='blog/index.html',
+                  request=request)
