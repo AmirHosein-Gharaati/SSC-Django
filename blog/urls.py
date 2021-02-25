8c87f602f327df1d1835642dff80756deada6c03
borna <borna@cafebazaar.ir>
2021-02-26 02:33:14 +0330

    Add forms

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.contrib.auth.views import LoginView
+from django.urls import path, reverse
 
-from blog.views import all_posts
+from blog.views import all_posts, new_post
 
 urlpatterns = [
+    path('login/', LoginView.as_view(template_name='blog/login.html'), name='login'),
+    path('new_post/', new_post, name='new_post'),
+    path('<author>/', all_posts, name='author_posts'),
     path('', all_posts, name='all_posts'),
-    path('<author>', all_posts, name='author_posts')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 from blog.models import Post
@@ -11,3 +13,18 @@ def all_posts(request, author=None):
     return render(context={'posts': posts},
                   template_name='blog/index.html',
                   request=request)
+
+
+def new_post(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        if not request.user.is_authenticated:
+            author = request.POST.get('author')
+        else:
+            author = request.user.username
+        post = Post(title=title, content=content, author=author)
+        post.save()
+        return HttpResponseRedirect('/blogs')
+
+    return render(request, 'blog/new_post.html')
