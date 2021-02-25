c2b70955f1531e666ee3373817b3c4bb8ce04dcb
borna <borna@cafebazaar.ir>
2021-02-26 01:36:06 +0330

    Add href to author in list

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from blog.views import all_posts
 
 urlpatterns = [
-    path('', all_posts),
-    path('<author>', all_posts)
+    path('', all_posts, name='all_posts'),
+    path('<author>', all_posts, name='author_posts')
 ]
